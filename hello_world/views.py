d3a77b36f9462472bd23c6962c33ff6e664229ea
Alicia <alicia.westerlund@outlook.com>
2025-11-03 12:20:36 +0100

    Changed broken code in views.py hello_world app

--- hello_world/views.py
+++ hello_world/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-
 def index(request):
-    return HttpResponse("Hello, World!")
+    """Simple index view that responds differently to POST vs other methods.
+
+    - POST -> short confirmation message
+    - GET/other -> Hello world message
+    """
+    if request.method == 'POST':
+        return HttpResponse("You must have POSTed something.")
+    else:
+        return HttpResponse(request.method)
